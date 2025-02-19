2f246f2bc671d7e2c2a4e23f000148e376804b3e
Samuel Laulhau <samuel.laulhau@puppets.fr>
2025-02-19 15:25:45 +0100

    typo in updatedb command on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ if [ ! -f $ONCE_FLAG ]; then
 
   drush-www maint:set 1
   drush-www cache:rebuild
-  drush-www update:database
+  drush-www updatedb
   drush-www config:import -y
   drush-www locale:update
   drush-www maint:set 0
