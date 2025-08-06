d26ca5cf5a91d58eee7e58ca19b9a536d180279e
arnaud <arnaud.mercier@puppets.fr>
2025-08-06 09:56:00 +0200

    Add namespace environment variable

--- scripts/load-azure-secrets.sh
+++ scripts/load-azure-secrets.sh
@@ -7,6 +7,10 @@ set -ex
 # load secrets from keyvault
 if [[ "${KEYVAULT}" && ! -f $ONCE_FLAG ]]; then
 
+  if [[ "${SECRET_NAMESPACE}" ]]; then
+      $KEYVAULT=${SECRET_NAMESPACE}_${KEYVAULT}
+  fi
+
   # Use managed identity to get an access token
   MI_API_VERSION="2019-08-01"
   ACCESS_TOKEN="$(curl -H "X-IDENTITY-HEADER: $IDENTITY_HEADER" -H 'Metadata: true' "$IDENTITY_ENDPOINT?api-version=$MI_API_VERSION&resource=https%3A%2F%2Fvault.azure.net" | jq -r .access_token)"
@@ -26,7 +30,7 @@ if [[ "${KEYVAULT}" && ! -f $ONCE_FLAG ]]; then
       echo $SECRETS_API_RESPONSE | jq -r .error.message
       exit 1
   fi
-  
+
   SECRETS=$(echo $SECRETS_API_VALUE | jq -r '.[].id')
 
   if [ -z "$SECRETS" ]; then
@@ -36,7 +40,7 @@ if [[ "${KEYVAULT}" && ! -f $ONCE_FLAG ]]; then
 
   SECRET_PREFIX="https://$KEYVAULT.vault.azure.net/secrets/"
   for SECRET_ID in $SECRETS
-  do 
+  do
     # Use the secret value (here we're just echoing it, but you'd typically use it in your application)
     echo "Retrieved secret: $SECRET_ID"
     SECRET_VALUE=$(curl -s -H "Authorization: Bearer $ACCESS_TOKEN" "$SECRET_ID?api-version=$SECRET_API_VERSION" | jq -r .value)
