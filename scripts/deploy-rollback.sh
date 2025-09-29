9e742e6dc9e2816acf6774c617221c7b75027bf0
Samuel Laulhau <sam@lalop.co>
2025-09-29 18:00:42 +0200

    rollback maint set

--- scripts/deploy-rollback.sh
+++ scripts/deploy-rollback.sh
@@ -7,3 +7,6 @@ BASEPATH=/opt/drupal
 if [ -f $BASEPATH/premep.sql ]; then
     drush-www sql:query --file={$BASEPATH}/premep.sql
 fi
+
+drush-www maint:set 0
+drush-www cache:rebuild
--- scripts/deploy.php
+++ scripts/deploy.php
@@ -37,7 +37,7 @@ if (!$isSuccessful) {
   if (file_exists("{$appRoot}/premep.sql")) {
     // restore database
     echo "###### Rolback deploy\n";
-    run(["drush-www", "sql:query", "--file={$appRoot}/premep.sql"]);
+    $isSuccessful = run(["bash", "scripts/deploy-rollback.sh"]);
   }
 
   throw new Exception("Error on deploy script");
