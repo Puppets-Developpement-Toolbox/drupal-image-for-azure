5aad27d9d747c441b1e311ac7b131507d13657ec
arnaud <arnaud.mercier@puppets.fr>
2026-03-05 15:56:02 +0100

    fix : syntaxe

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,32 +12,32 @@ ACTION=$1
 
 BASEPATH=/opt/drupal
 
-if [ "$ACTION" = "maint1"] then
+if [ "$ACTION" = "maint1"]; then
     drush-www maint:set 1
 fi
-if [ "$ACTION" = "dump"] then
+if [ "$ACTION" = "dump"]; then
     drush-www sql:dump --gzip --result-file=$BASEPATH/storage/premep.sql --structure-tables-list=cache,cache_*
 fi
-if [ "$ACTION" = "updb"] then
+if [ "$ACTION" = "updb"]; then
     drush-www updatedb
 fi
-if [ "$ACTION" = "cim"] then
+if [ "$ACTION" = "cim"]; then
     drush-www config:import -y
 fi
-if [ "$ACTION" = "localupd"] then
+if [ "$ACTION" = "localupd"]; then
     drush-www locale:update
 fi
-if [ "$ACTION" = "maint0"] then
+if [ "$ACTION" = "maint0"]; then
     drush-www maint:set 0
 fi
-if [ "$ACTION" = "cr"] then
+if [ "$ACTION" = "cr"]; then
     drush-www cache:rebuild
 fi
 
-if [ "$ACTION" = "ver"] then
+if [ "$ACTION" = "ver"]; then
     echo $APP_VERSION > $BASEPATH/storage/private/deployed_version
 fi
 
-if [ "$ACTION" = "rm"] then
+if [ "$ACTION" = "rm"]; then
     rm $BASEPATH/web/deploy.php
 fi
