dbe1f1303f5983b73b8c46b74053ae5a9ac59526
arnaudPuppets <arnaud.mercier@puppets.fr>
2026-03-06 12:03:14 +0100

    Update docker-entrypoint.sh syntaxe

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -7,7 +7,7 @@ load-azure-secrets
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
 #environment for cron
-printenv >> /etc/environment
+printenv > /etc/environment
 
 # if we start apache then start cron, ssh and launch deploy
 if [ "${1#-}" != "$1" ] || [ "${1#apache2-foreground}" != "$1" ]; then
