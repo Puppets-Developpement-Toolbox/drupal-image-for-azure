a186b935622083d070ab57728a889f995d3c21f8
Samuel Laulhau <samuel.laulhau@puppets.fr>
2026-04-27 21:09:52 +0200

    Fix environment variable retrieval in entrypoint script

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,7 +5,7 @@ load-azure-secrets
 
 if [ ! -f /etc/profile.d/azure-env.sh ]; then
   # Retrieve the environment variables to propagate them in cron and the SSH session.
-  cat /proc/1/environ | tr '\0' '\n' | grep -v '^$' | \
+  printenv | \
     grep -vE '^(PATH|HOME|HOSTNAME|TERM|SHLVL|PWD|_)=' | \
     sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' \
     >> /etc/environment
