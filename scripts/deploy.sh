4245a99d65cd0eeb57082e0d0e2c8822837fd963
arnaudPuppets <arnaud.mercier@puppets.fr>
2026-09-25 14:57:48 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,6 @@ BASEPATH=/opt/drupal
 
 if [ "$ACTION" = "maint1" ]; then
     drush-www maint:set 1
-    drush-www cache:rebuild
 fi
 if [ "$ACTION" = "dump" ]; then
     # Pour éviter le timeout de Azure
